e7ba63ce0e18e89cae2d4d27ccdddcb12cffd888
Matthew Riley <mdriley@gmail.com>
2024-11-30 23:35:14 -0800

    day 1 successful

---
+++ 01.py
@@ -0,0 +1,33 @@
+from collections import Counter
+
+a1: list[int] = []
+a2: list[int] = []
+
+with open('inputs/01.txt') as f:
+  for l in f:
+    fields = l.split()
+    a1.append(int(fields[0]))
+    a2.append(int(fields[1]))
+
+a1 = sorted(a1)
+a2 = sorted(a2)
+
+# part 1
+
+s = 0
+
+for (x, y) in zip(a1, a2):
+  s += abs(x - y)
+
+print(s)
+
+# part 2
+
+c2 = Counter(a2)
+
+s2 = 0
+
+for n in a1:
+  s2 += n * c2[n]
+
+print(s2)
