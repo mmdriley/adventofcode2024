fd645bd0c66d142142df8cba2cb57611f08db7c8
Matthew Riley <mdriley@gmail.com>
2024-12-11 01:31:50 -0800

    Successful day 11
    Dynamic programming in Python almost feels like cheating

--- 11/11.py
+++ 11/11.py
@@ -1,25 +1,28 @@
-from typing import Iterable
+from functools import cache
 
 with open('11.txt') as f:
   stones = [int(x) for x in f.read().strip().split()]
 
-def blink(n: int) -> list[int]:
+@cache
+def blinklen(n: int, remaining: int) -> int:
+  if remaining == 0:
+    return 1
+
   if n == 0:
-    return [1]
+    return blinklen(1, remaining - 1)
   
   sn = str(n)
   if len(sn) % 2 == 0:
-    return [int(sn[:len(sn)//2]), int(sn[len(sn)//2:])]
+    sn1 = sn[:len(sn)//2]
+    sn2 = sn[len(sn)//2:]
+    return blinklen(int(sn1), remaining - 1) + blinklen(int(sn2), remaining - 1)
 
-  return [n * 2024]
+  return blinklen(2024 * n, remaining - 1)
 
-def flatten(ll: list[list[int]]) -> Iterable[int]:
-  for l in ll:
-    for x in l:
-      yield x
+# part 1
 
-for i in range(75):
-  print(i)
-  stones = list(flatten([blink(n) for n in stones]))
+print(sum([blinklen(n, 25) for n in stones]))
 
-print(len(stones))
+# part 2
+
+print(sum([blinklen(n, 75) for n in stones]))
