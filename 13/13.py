36251fd4e67e9a46c4d6c9a6947b588d0fc29eee
Matthew Riley <mdriley@gmail.com>
2024-12-13 16:16:48 -0800

    Successful 13.2
    Haha it was algebra

--- 13/13.py
+++ 13/13.py
@@ -1,16 +1,15 @@
 # a machine is defined by:
 # A+X, A+Y, B+X, B+Y, prize.X, prize.Y
 
-from functools import cache
 import re
 
 with open('13.txt') as f:
   machine_descriptions = f.read()
 
-machine_regex = re.compile(
-  r"""Button A: X\+(\d+), Y\+(\d+)
+machine_regex = re.compile(r"""
+Button A: X\+(\d+), Y\+(\d+)
 Button B: X\+(\d+), Y\+(\d+)
-Prize: X=(\d+), Y=(\d+)""")
+Prize: X=(\d+), Y=(\d+)""".lstrip())
 
 machines = []
 
@@ -18,52 +17,38 @@ for m in machine_regex.finditer(machine_descriptions):
   machines.append(tuple(int(k) for k in m.groups()[0:7]))
 
 
-impossible_sentinel = -2, -2
+# pressing A costs three tokens; pressing B costs one token
 
+def spend_or_zero(ax, ay, bx, by, prizeX, prizeY):
+  # use Cramer's rule to solve a system of two linear equations
+  detA = (ax * by) - (bx * ay)
+  detA1 = (prizeX * by) - (bx * prizeY)
+  detA2 = (ax * prizeY) - (prizeX * ay)
 
-def cost(solve: tuple[int, int]) -> int:
-  n_a, n_b = solve
-  return 3 * n_a + 1 * n_b
+  n = detA1 / detA
+  k = detA2 / detA
 
+  # no solution if it would require a fractional number of button presses
+  if n != int(n) or k != int(k):
+    return 0
 
-# returns A presses, B presses
-@cache
-def mincostsolve(ax, ay, bx, by, prizeX, prizeY, max_presses) -> tuple[int, int]:
-  if max_presses <= 0:
-    return impossible_sentinel
-
-  if prizeX < 0 or prizeY < 0:
-    return impossible_sentinel
-
-  if prizeX == 0 and prizeY == 0:
-    return 0, 0
-  
-  solve_a = solve_b = impossible_sentinel
-
-  aa, ab = mincostsolve(ax, ay, bx, by, prizeX - ax, prizeY - ay, max_presses - 1)
-  if aa < max_presses and ab >= 0:
-    solve_a = (aa + 1, ab)
-
-  ba, bb = mincostsolve(ax, ay, bx, by, prizeX - bx, prizeY - by, max_presses - 1)
-  if bb < max_presses and ba >= 0:
-    solve_b = (ba, bb + 1)
-
-  if solve_a == impossible_sentinel:
-    return solve_b
-  elif solve_b == impossible_sentinel:
-    return solve_a
-  else:
-    return solve_a if cost(solve_a) < cost(solve_b) else solve_b
+  return int(3*n + 1*k)
 
 
 # part 1
 
 spend1 = 0
 for ax, ay, bx, by, prizeX, prizeY in machines:
-  sa, sb = mincostsolve(ax, ay, bx, by, prizeX, prizeY, 200)
-  if sa >= 0:
-    spend1 += cost((sa, sb))
-
-    
+  spend1 += spend_or_zero(ax, ay, bx, by, prizeX, prizeY)
 
 print(spend1)
+
+
+# part 2
+
+spend2 = 0
+for ax, ay, bx, by, prizeX, prizeY in machines:
+  spend2 += spend_or_zero(ax, ay, bx, by,
+                          prizeX + 10000000000000, prizeY + 10000000000000)
+
+print(spend2)
