6eca1c4f5c25a1b5bbbed9fd122c7e5af2c65b33
Matthew Riley <mdriley@gmail.com>
2024-12-19 00:38:20 -0800

    Successful day 19
    Yep, RE2 was too clever by half.

--- 19/19.py
+++ 19/19.py
@@ -1,3 +1,4 @@
+from functools import cache
 import re2 as re
 
 designs = []
@@ -8,6 +9,35 @@ with open('19.txt') as f:
   for l in f:
     designs.append(l.strip())
 
-possiblere = re.compile('^(' + '|'.join(patterns) + ')*$')
 
-print(sum([1 if possiblere.match(d) else 0 for d in designs]))
+@cache
+def ways_to_make(design: str):
+  if design == '':
+    return 1
+
+  s = 0
+  for p in patterns:
+    if design.startswith(p):
+      s += ways_to_make(design[len(p):])
+
+  return s
+
+
+# part 1
+
+possible = 0
+
+for d in designs:
+  if ways_to_make(d) > 0:
+    possible += 1
+
+print(possible)
+
+# part 2
+
+ways = 0
+
+for d in designs:
+  ways += ways_to_make(d)
+
+print(ways)
