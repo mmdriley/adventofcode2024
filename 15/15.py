1c2c16dff34d78b0cfc7f0678e04252fef4fbeee
Matthew Riley <mdriley@gmail.com>
2024-12-20 15:48:12 -0800

    Finally finished 15.2
    Solution is slow, though. Too many copies.

--- 15/15.py
+++ 15/15.py
@@ -25,6 +25,57 @@ movedirs = {
   '<': (0, -1),
 }
 
+# (i, j) is a part of a box to be pushed by (di, dj)
+def pushbox(m: list[list[str]], boxi: int, boxj: int, di: int, dj: int) -> bool:
+  match m[boxi][boxj]:
+    case 'O':
+      old_coords = [(boxi, boxj)]
+    case '[':
+      assert(m[boxi][boxj+1] == ']')
+      old_coords = [(boxi, boxj), (boxi, boxj+1)]
+    case ']':
+      assert(m[boxi][boxj-1] == '[')
+      old_coords = [(boxi, boxj-1), (boxi, boxj)]
+    case _:
+      raise ValueError()  # not a box
+
+  # make room for this box
+  for oldi, oldj in old_coords:
+    newi = oldi + di
+    newj = oldj + dj
+
+    match m[newi][newj]:
+      case '.':
+        pass  # nothing to do
+      case '[' if dj == -1:
+        assert(m[oldi][oldj] == ']')
+      case ']' if dj == 1:
+        assert(m[oldi][oldj] == '[')
+      case '#':
+        return False  # hit a wall, return failure
+      case '[' | ']' | 'O' as boxchar:
+        # We ignored the cases where boxes were pushing into themselves, so
+        # this is pushing into a new box. Try pushing it.
+        if not pushbox(m, newi, newj, di, dj):
+          return False
+        assert(m[newi][newj] == '.')
+
+  # take care to capture the old chars, erase the old coords,
+  # then write the new ones, to avoid ever reading our own writes
+  # within a loop
+  old_chars = [m[oldi][oldj] for (oldi, oldj) in old_coords]
+
+  for (oldi, oldj) in old_coords:
+    m[oldi][oldj] = '.'
+
+  for ((oldi, oldj), c) in zip(old_coords, old_chars):
+    newi = oldi + di
+    newj = oldj + dj
+
+    m[newi][newj] = c
+
+  return True
+
 
 def runmap(warehousemap, moves, roboti, robotj):
   warehousemap = copy.deepcopy(warehousemap)
@@ -32,6 +83,7 @@ def runmap(warehousemap, moves, roboti, robotj):
   # print('\n'.join([''.join(l) for l in warehousemap]))
 
   for m in moves:
+    # print(m)
     assert(warehousemap[roboti][robotj] == '@')
     if m == '\n':
       continue
@@ -50,20 +102,12 @@ def runmap(warehousemap, moves, roboti, robotj):
       if nextc == '.':
         pass
       else:
-        assert(nextc == 'O')
-        # trying to push one or more boxes
-        afteri, afterj = nexti, nextj
-
-        while warehousemap[afteri][afterj] == 'O':
-          afteri += movei
-          afterj += movej
-
-        if warehousemap[afteri][afterj] == '#':
-          continue  # can't push boxes into a wall
+        assert(nextc in ['[', ']', 'O'])
+        mapafter = copy.deepcopy(warehousemap)
+        if pushbox(mapafter, nexti, nextj, movei, movej):
+          warehousemap = mapafter
         else:
-          assert(warehousemap[afteri][afterj] == '.')
-          warehousemap[afteri][afterj] = 'O'
-          warehousemap[nexti][nextj] = '.'
+          continue  # can't push box
 
       # do the move
       assert(warehousemap[nexti][nextj] == '.')
@@ -71,8 +115,7 @@ def runmap(warehousemap, moves, roboti, robotj):
       roboti, robotj = nexti, nextj
       warehousemap[roboti][robotj] = '@'
 
-  # print(m)
-  # print('\n'.join([''.join(l) for l in warehousemap]))
+    # print('\n'.join([''.join(l) for l in warehousemap]))
   return warehousemap
 
 
@@ -80,7 +123,7 @@ def gpssum(warehousemap):
   gpssum = 0
   for i, l in enumerate(warehousemap):
     for j, c in enumerate(l):
-      if c == 'O':
+      if c in ['O', '[']:
         gpssum += i * 100 + j
 
   return gpssum
@@ -109,3 +152,6 @@ for i in range(len(part2map)):
 
 roboti, robotj = roboti_start, robotj_start * 2
 assert(part2map[roboti][robotj] == '@')
+
+part2map = runmap(part2map, moves, roboti, robotj)
+print(gpssum(part2map))
